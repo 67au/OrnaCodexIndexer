3eb6fcc78dbd4d6a993d5f5f31fd833946fda2b9
67au <oyy6745@gmail.com>
2023-03-15 22:41:40 +0800

    fix: fix element xpath

--- codex_parser/page_parser.py
+++ codex_parser/page_parser.py
@@ -8,8 +8,8 @@ from .codex_types import CodexType
 
 EFFECT_PATTERN = r'(?P<EFFECT>.+) \((?P<CHANCE>\d+%)\)'
 effect_pattern = re.compile(pattern=EFFECT_PATTERN) # type: ignore
-META_PATTERN = r'(?P<KEY>.+)(:|：) (?P<VALUE>.+)'
-meta_pattern = re.compile(pattern=META_PATTERN) # type: ignore
+KV_PATTERN = r'(?P<KEY>.+)(:|：) (?P<VALUE>.+)'
+kv_pattern = re.compile(pattern=KV_PATTERN) # type: ignore
 
 class PageParser:
 
@@ -23,25 +23,69 @@ class PageParser:
                 drop[drop_header] = []
                 handler = drop[drop_header]
             elif elem.attrib.get('class') == 'drop':
+                drop_str = elem.xpath("string()").strip() 
+                icon = elem.xpath('img')[0].attrib.get('src')
+                ability = elem.xpath('../div[@class="emph"]')
                 if elem.tag == 'a':
                     handler.append({
                         'href': elem.attrib.get('href'), 
-                        'name': elem.xpath("string()").strip(),
+                        'name': drop_str,
+                        'icon': icon,
                     })
                 else:
-                    matches = effect_pattern.match(elem.xpath("string()").strip())
+                    matches = effect_pattern.match(drop_str)
                     if matches:
-                        handler.append([matches.group('EFFECT'), matches.group('CHANCE')])
+                        handler.append({
+                            'name':matches.group('EFFECT'), 
+                            'chance': matches.group('CHANCE'),
+                            'icon': icon,
+                        })
+                    elif ability:
+                        handler.append({
+                            'name': drop_str, 
+                            'icon': icon,
+                            'ability': ability[0].xpath("string()").strip(),
+                        })
+                    else:
+                        handler.append({'name': drop_str, 'icon': icon})
         return dict(drop)
     
 
     @classmethod
     def kv_parse_iter(cls, elems: Iterator):
         for elem in elems:
-            meta = elem.xpath("string()").strip()
-            matches = meta_pattern.match(meta)
+            kv = elem.xpath("string()").strip()
+            matches = kv_pattern.match(kv)
             if matches:
-                yield [matches.group('KEY'), matches.group('VALUE')]
+                yield ['_'.join(matches.group('KEY').split()), matches.group('VALUE')]
+            else:
+                yield kv
+    
+    @classmethod
+    def meta_parse_iter(cls, elems: list):
+        for elem in elems:
+            span = elem.xpath('span')
+            if len(span) > 0 and span[0].xpath('boolean(@class="exotic")'):
+                yield ['exotic', span[0].xpath("string()").strip()]
+                continue
+            meta = elem.xpath("string()").strip()
+            matches = kv_pattern.match(meta)
+            if matches:
+                yield ['_'.join(matches.group('KEY').split()), matches.group('VALUE')]
+            else:
+                yield meta
+
+    @classmethod
+    def stat_parse_iter(cls, elems: list):
+        for elem in elems:
+            stats = elem.attrib.get('class').split()
+            if len(stats) > 1:
+                yield [f'element:{stats[1]}', elem.xpath("string()").strip()]
+                continue
+            meta = elem.xpath("string()").strip()
+            matches = kv_pattern.match(meta)
+            if matches:
+                yield ['_'.join(matches.group('KEY').split()), matches.group('VALUE')]
             else:
                 yield meta
 
@@ -49,15 +93,19 @@ class PageParser:
     def description_parse(cls, pre_elems: list, div_elems: list, codex_type: str) -> Tuple[str, list, list]:
         description = pre_elems[0].xpath("string()").strip() if len(pre_elems) > 0 else ''
         meta_extra = []
-        stat_extra = []
+        offhand = []
         if codex_type in {'items'} and len(div_elems) > 0:
-            stat_extra = [[div_elems[0].xpath("preceding-sibling::div[1]")[0].xpath("string()").strip(), div_elems[0].xpath("string()").strip()]]
-        if codex_type in {'bosses', 'monster'} and len(div_elems) > 0:
+            ability = div_elems[0].xpath("preceding-sibling::div[1]")[0].xpath("string()").strip().split(':')
+            offhand = [ability[0], [{
+                'name': ability[1],
+                'ability': div_elems[0].xpath("string()").strip(),
+            }]]
+        if codex_type in {'bosses', 'monsters'} and len(div_elems) > 0:
             meta_extra = list(cls.kv_parse_iter(div_elems)) # type: ignore
         if codex_type in {'followers', 'raids', 'spells', 'classes'} and len(div_elems) > 0:
             description = div_elems[0].xpath("string()").strip()
             meta_extra = list(cls.kv_parse_iter(div_elems[1:])) # type: ignore
-        return description, meta_extra, stat_extra
+        return description, meta_extra, offhand
 
     @classmethod
     def parse(cls, html: str, codex: str, raw_dict: bool = False) -> Union[dict, CodexType, None]:
@@ -73,23 +121,25 @@ class PageParser:
         icon = icon_elem.attrib['src']
 
         description_pre_elems = page.xpath('/html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]/pre[contains(@class,"codex-page-description")]')
-        description_div_elems = page.xpath('/html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]//div[contains(@class,"codex-page-description")]')
-        description, meta_extra, stat_extra = cls.description_parse(description_pre_elems, description_div_elems, codex.split('/')[2])
+        description_div_elems = page.xpath('/html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]/div[contains(@class,"codex-page-description")]')
+        description, meta_extra, offhand = cls.description_parse(description_pre_elems, description_div_elems, codex.split('/')[2])
 
         meta_elems = page.xpath('/html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]//div[@class="codex-page-meta"]')
-        meta = meta_extra + list(cls.kv_parse_iter(meta_elems))
+        meta = meta_extra + list(cls.meta_parse_iter(meta_elems))
 
         tag_elems = page.xpath('/html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]//div[@class="codex-page-tag"]')
-        tag = list(cls.kv_parse_iter(tag_elems))
+        tag = [i[2:] for i in cls.kv_parse_iter(tag_elems)]
 
         stat_elems = page.xpath('/html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]/div[@class="codex-stats"]//div[contains(@class,"codex-stat")]')
-        stat = list(cls.kv_parse_iter(stat_elems)) + stat_extra
+        stat = list(cls.stat_parse_iter(stat_elems))
 
         drop_elems = page.xpath(
             '/html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]/h4'
-            '| /html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]//*[@class="drop"]'
+            '|/html/body/div[@class="wraps"]/div[@class="page"]/div[@class="codex-page"]//*[@class="drop"]'
         )
         drop = cls.drop_parse(drop_elems)
+        if offhand:
+            drop[offhand[0]] = offhand[1]
 
         if raw_dict:
             return {
